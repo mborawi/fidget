b30513a2c2547d39ec22ee014e449271518db427
mborawi <mborawi@gmail.com>
2019-03-27 13:52:40 +1100

    added vue files

---
+++ dashboard.vue
@@ -0,0 +1,257 @@
+<template>
+  <v-container grid-list-md fluid >
+    <v-layout
+      row
+      wrap
+    >
+      <v-flex xs4>
+          <v-toolbar color="#254351" dark>
+            <v-toolbar-title>Weekly Top Performers</v-toolbar-title>
+          </v-toolbar>
+          <v-list two-line>
+              <v-subheader>
+                A list of staff who were precieved by peers to perform well this week
+              </v-subheader>
+            <template v-for="(item, index) in top_weekly">
+              <v-list-tile
+                :key="item.title"
+                avatar
+                ripple
+                @click="toggle(index)"
+              >
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+                  <v-list-tile-sub-title class="text--primary">{{ item.job_title }}</v-list-tile-sub-title>
+                  <v-list-tile-sub-title >{{ item.bsl }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-action>
+                  <v-btn
+                    small
+                    fab
+                    flat
+                    class="grey"
+                    outline
+                    disabled
+                  >
+                    <span class="grey--text">{{item.score}}</span>
+                  </v-btn>
+
+                  
+                </v-list-tile-action>
+
+              </v-list-tile>
+              <v-divider
+                v-if="index + 1 < top_weekly.length"
+                :key="index"
+              ></v-divider>
+            </template>
+          </v-list>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs4>
+          <v-toolbar color="#00BA9F" dark>
+            <v-toolbar-title>Consistent Top Performers</v-toolbar-title>
+          </v-toolbar>
+          <v-list two-line>
+            <v-subheader>
+              A list of top staff who appeared on top lists and count of times
+            </v-subheader>
+
+            <template v-for="(item, index) in top">
+              <v-list-tile
+                :key="item.title"
+                avatar
+                ripple
+                @click="toggle(index)"
+              >
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+                  <v-list-tile-sub-title class="text--primary">{{ item.job_title }}</v-list-tile-sub-title>
+                  <v-list-tile-sub-title >{{ item.bsl }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-action>
+                  <v-btn
+                    small
+                    fab
+                    flat
+                    class="grey"
+                    outline
+                    disabled
+                  >
+                    <span class="grey--text">{{item.count}}</span>
+                  </v-btn>
+
+                  
+                </v-list-tile-action>
+
+              </v-list-tile>
+              <v-divider
+                v-if="index + 1 < top_weekly.length"
+                :key="index"
+              ></v-divider>
+            </template>
+          </v-list>
+        </v-flex>
+        <v-flex xs4>
+            <v-toolbar color="#A5CD2B" dark>
+              <v-toolbar-title>Top BSL Teams</v-toolbar-title>
+            </v-toolbar>
+            <v-list>
+              <v-subheader>
+                Best Recognised teams
+              </v-subheader>
+              <template v-for="(item, index) in top_teams">
+                <v-list-tile
+                  :key="item.name"
+                  avatar
+                  ripple
+                  @click="toggle(index)"
+                >
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+                  </v-list-tile-content>
+
+                  <v-list-tile-action>
+                    <v-btn
+                      small
+                      fab
+                      flat
+                      class="grey"
+                      outline
+                      disabled
+                    >
+                      <span class="grey--text">{{item.score}}</span>
+                    </v-btn>
+
+                    
+                  </v-list-tile-action>
+
+                </v-list-tile>
+                <v-divider
+                  v-if="index + 1 < top_teams.length"
+                  :key="index"
+                ></v-divider>
+              </template>
+            </v-list>
+          </v-flex>
+          
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    methods: {
+         toggle (index) {
+           const i = this.selected.indexOf(index)
+
+           if (i > -1) {
+             this.selected.splice(i, 1)
+           } else {
+             this.selected.push(index)
+           }
+         }
+    },
+    data: () => ({
+      selected: [2],
+      top_teams:[
+        {
+          name: "ATO Innovation",
+          score: 22
+        },
+        {
+          name: "Phoenix",
+          score: 20
+        },
+        {
+          name: "Enterprise Testing",
+          score: 17
+        },
+        {
+          name: "Risk And Intelligence",
+          score: 14
+        },
+        {
+          name: "Workplace Diversity",
+          score: 12
+        },
+        {
+          name: "Superfunds",
+          score: 8
+        },
+      ],
+      top:[
+        {
+          bsl: 'IAL',
+          job_title: 'EL1 Finance Manager',
+          name: 'Sharon Krucler',
+          count: "26"
+        },
+        {
+          bsl: 'TCN',
+          job_title: 'APS6 Client Engagement',
+          name: 'Gavin Grace',
+          count: "15"
+        },
+        {
+          bsl: 'ESD',
+          job_title: 'EL2 Debt Collection Team Leader',
+          name: 'Dean Drum',
+          count: "12"
+        },
+        {
+          bsl: 'BRR',
+          job_title: 'SES2 Deputy Commissioner',
+          name: 'Marcus Zhang',
+          count: "11"
+        },
+        {
+          bsl: 'ECCI',
+          job_title: 'APS6 Data Scientist',
+          name: 'Rebecca Dunn',
+          count: "8"
+        }
+      ],
+      top_weekly: [
+        {
+          bsl: 'ATOF',
+          job_title: 'EL1 Finance Manager',
+          name: 'Ali Connors',
+          score: "8.9"
+        },
+        {
+          bsl: 'SDP',
+          job_title: 'APS6 Client Engagement',
+          name: 'Jennifer Smith',
+          score: "8.8"
+        },
+        {
+          bsl: 'PGI',
+          job_title: 'EL2 Debt Collection Team Leader',
+          name: 'Sandra Adams',
+          score: "8.6"
+        },
+        {
+          bsl: 'EST',
+          job_title: 'SES2 Deputy Commissioner',
+          name: 'Trevor Hansen',
+          score: "8.4"
+        },
+        {
+          bsl: 'SMB',
+          job_title: 'APS6 Data Scientist',
+          name: 'Britta Holt',
+          score: "8.3"
+        }
+      ]
+    })
+  }
+</script>
+
+<style>
+
+</style>
---
+++ input.vue
@@ -0,0 +1,187 @@
+<template>
+  <v-container grid-list-md fluid >
+    <v-flex xs12>
+      <v-card class="indigo lighten-4">
+        <v-card-title primary-title>
+          <div>
+            <div class="display-1 mb-0 indigo--text text--darken-2">Skit Dashhood <span class="headline mb-0 grey--text text--darken-3" > WFA ATOP </span> 
+            </div>
+
+            <div class="subheading grey--text text--darken-3"> 
+              <v-icon color="indigo darken-2">home</v-icon> 
+              EL2H Data Scientist
+            </div>
+            <div class="subheading grey--text text--darken-3"> <v-icon color="indigo darken-2">credit_card</v-icon> AGS  No.: 564228221</div>
+          </div>
+        </v-card-title>
+        
+      </v-card>
+    </v-flex>
+    <v-container grid-list-md fluid >
+      <v-layout row wrap>
+        <v-flex xs4 class="amber lighten-4 pa-3">
+          <v-data-table
+          disable-initial-sort
+            hide-actions
+            :headers="headers"
+            :items="performances"
+            class="elevation-1"
+          >
+            <template v-slot:items="props" >
+              <tr :class="getColor(props.item.agg_score,props.item.own_score )">
+              <td>{{ props.item.week_date }}</td>
+              <td class="text-xs-right" >{{ props.item.agg_score }}</td>
+              <td class="text-xs-right">{{ props.item.own_score }}</td>
+              </tr>
+            </template>
+          </v-data-table>
+        </v-flex>
+        <v-flex xs8 elevation=1>
+          <v-card>
+              <v-toolbar card dense class="amber lighten-2 red--text">
+                <v-toolbar-title>
+                  <span class="title text-uppercase">Performance</span>
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-checkbox v-model="EndabledAll" label="Score all" color="red"></v-checkbox>
+              </v-toolbar>
+
+              <v-card-text>
+                <v-layout justify-space-between  mb-3>
+                  <v-flex text-xs-left>
+                    <v-slider
+                    v-model="evaluation.self"
+                    color="green darken-2"
+                    label="Self Evaluation"
+                    max=10
+                    step=0.5
+                    ticks="always"
+                    ></v-slider>
+                    <v-layout row>
+                    <v-flex class="pr-3">
+                    <v-slider
+                    v-model="evaluation.manager"
+                    color="blue darken-2"
+                    label="Carlos Sand"
+                    max=10
+                    step=0.5
+                    ticks="always"
+                    ></v-slider> 
+                  </v-flex>  
+                    <v-flex shrink style="width: 60px">
+                     <v-text-field
+                       v-model="evaluation.manager"
+                       class="mt-0"
+                       hide-details
+                       single-line
+                       type="number"
+                     ></v-text-field>
+                   </v-flex>
+                 </v-layout>
+                    <v-slider
+                    v-model="evaluation.colleague1"
+                    color="red darken-2"
+                    label="Dave Ludlum"
+                    max=10
+                    step=0.5
+                    ticks="always"
+                    ></v-slider>
+                    <v-slider
+                    v-model="evaluation.colleague2"
+                    color="red darken-2"
+                    label="Amy Dale"
+                    max=10
+                    step=0.5
+                    ticks="always"
+                    ></v-slider>
+                    <v-slider
+                    v-model="evaluation.subord1"
+                    color="amber darken-2"
+                    label="Mike Adams"
+                    max=10
+                    step=0.5
+                    ticks="always"
+                    ></v-slider>
+                  </v-flex>
+                </v-layout>
+
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="green darken-3 white--text" >Submit<v-icon right dark>check_circle_outline</v-icon></v-btn>
+                <v-btn color="red darken-3 white--text" >Clear<v-icon right dark>cancel</v-icon></v-btn>
+              </v-card-actions>
+            </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    methods: {
+      getColor:function(own,total){
+        if (own > total){
+          return "red--text text-lighten-2";
+        }else if (total > own){
+          return "green--text text-lighten-2";
+        }
+        return "green lighten-4 green--text text-lighten-1";
+
+
+      }
+    },
+    data: () => ({
+      EndabledAll: true,
+      evaluation: {self:0, manager:0, colleague1:0,colleague2:0, subord1:0},
+      headers: [
+                { text: 'Week', value: 'week_date', align: 'center', class: 'green--text text--darken-3 subheading' },
+                { text: 'Total Score', value: 'agg_score', align: 'center', class: 'green--text text--darken-3 subheading' },
+                { text: 'Self Evaluation', value: 'own_score' , align: 'centre', class: 'green--text text--darken-3 subheading'}
+              ],
+      performances: [
+        {
+          week_date: "25-Mar-2019",
+          agg_score: 8.5,
+          own_score: 8,
+        },
+        {
+          week_date: "18-Mar-2019",
+          agg_score: 9.1,
+          own_score: 8.4,
+        },
+        {
+          week_date: "11-Mar-2019",
+          agg_score: 7,
+          own_score: 6.8,
+        },
+        {
+          week_date: "25-Feb-2019",
+          agg_score: 9,
+          own_score: 9,
+        },
+        {
+          week_date: "11-Feb-2019",
+          agg_score: 8,
+          own_score: 9,
+        },
+        {
+          week_date: "04-Feb-2019",
+          agg_score: 8,
+          own_score: 9,
+        },
+        {
+          week_date: "28-Jan-2019",
+          agg_score: 8,
+          own_score: 9,
+        }
+      ],
+
+
+    })
+  }
+</script>
+
+<style>
+
+</style>
